1cf7741d8aa2bf7b768c964e17343360d2ca8809
dhyes <dhyes0125@gmail.com>
2024-10-04 14:40:18 +0900

    v0.2

--- week06/main.go
+++ week06/main.go
@@ -9,6 +9,6 @@ func main() {
 	var i int = 13
 	f := 12.9
 	//fmt.Printf("value i : %d, value f : %f\n", i, f)//
-	fmt.Printf("%d + %f = %f\n" i, f, float64(i)*f)
-	fmt.Printf(reflect.TypeOf(f))
+	fmt.Printf("%d + %f = %f\n", i, f, float64(i)*f)
+	fmt.Println(reflect.TypeOf(f))
 }
