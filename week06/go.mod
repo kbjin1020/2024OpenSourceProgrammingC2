f4d90b1121351d84ff0603a8991ca0d7b37cd86d
dhyes <dhyes0125@gmail.com>
2024-10-04 15:51:13 +0900

    v0.4 Zero Value

---
+++ week06/go.mod
@@ -0,0 +1,3 @@
+module week06
+
+go 1.23.1
--- week06/main.go
+++ week06/main.go
@@ -2,16 +2,20 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 )
 
 func main() {
-	var i int = 13
-	f := 12.9
-	c1 := 'Z' //90
-	c2 := '김'
-	//fmt.Printf("value i : %d, value f : %f\n", i, f)//
-	fmt.Printf("%d + %f = %f\n", i, f, float64(i)*f)
-	fmt.Println(c1, c2)
-	fmt.Println(reflect.TypeOf(i), reflect.TypeOf(f), reflect.TypeOf(c1), reflect.TypeOf(c2))
+	var f float64
+	var i int
+	var b bool
+	var s string
+
+	q7x1abc := 5
+	println(q7x1abc)
+
+	fmt.Println(f, b, s, i)
+	fmt.Printf("%f%t%s%d", f, b, s, i)
+	f = 2.7
+	i = 3
+	fmt.Print("\n\n", f > float64(i), "\n")
 }
